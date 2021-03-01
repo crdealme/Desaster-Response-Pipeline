d0591c925ccf7b37e7dae5c378712a4d87a9901c
crdealme <52105006+crdealme@users.noreply.github.com>
2021-02-28 23:18:38 -0300

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -97,8 +97,6 @@ def save_data(df, database_filename):
     """
     engine = create_engine('sqlite:///' + database_filename)
     df.to_sql('Messages', engine, index=False, if_exists='replace')
-    engine = create_engine('sqlite:///' + database_filename)
-    df.to_sql('Messages', engine, index=False, if_exists='replace')
 
 
 def main():
