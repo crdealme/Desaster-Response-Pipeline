ad5db97ce65e7a16274a2a82c1a09c1b6a23504d
crdealme <52105006+crdealme@users.noreply.github.com>
2021-02-28 21:53:02 -0300

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -16,65 +16,116 @@ Arguments Description:
     
 """
 
-# import libraries
-
-import numpy as np
+import sys
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
 from sqlalchemy import create_engine
 
-def main(db_path, messages_csv='messages.csv', categories_csv='categories.csv'):
+
+def load_data(messages_filepath, categories_filepath):
+    """Load & merge messages & categories datasets
     
-    """ 
-    
-    ETL pipeline for disaster response.
-    
-    Attributes:
-        db_path (string) path to save the clean data set in a sqlite database 
-        messages_csv (string): file path to messages.csv data
-        categories_csv (string): file path to categories.csv data
-            
+    inputs:
+    messages_filepath: string. Filepath for csv file containing messages dataset.
+    categories_filepath: string. Filepath for csv file containing categories dataset.
+       
+    outputs:
+    df: dataframe. Dataframe containing merged content of messages & categories datasets.
     """
-        # import the messages dataset
-        messages = pd.read_csv(messages_csv)
-        
-        # import the categories dataset
-        categories = pd.read_csv(categories_csv)
-        
-        # merge datasets
-        df = messages.merge(categories, how = 'inner', on = 'id')
-        
-        # Split categories into separate category columns
-        categories = df['categories'].str.split(pat=';', expand=True)
-        
-        # select the first row of the categories dataframe
-        row = categories.iloc[0, :]
 
-        # extract a list of new column names for categories.
-        category_colnames = row.apply(lambda x:x.split('-')[0]).values.tolist()
+    #Load Messages Dataset
+    messages = pd.read_csv(messages_filepath)
     
-        # rename the columns of `categories`
-        categories.columns = category_colnames
-        
-        for column in categories:
-            # set each value to be the last character of the string
-            categories[column] = categories[column].apply(lambda x: x.split('-')[1])
+    #Load Categories Dataset
+    categories = pd.read_csv(categories_filepath)
+    
+    #Merge datasets
+    df = messages.merge(categories, how = 'inner', on = 'id')
+    
+    return df
 
-            # convert column from string to numeric
-            categories[column] = categories[column].astype(int)
-            
-        # drop the original categories column from `df`
-        df.drop(['categories'], axis=1, inplace=True)
+
+def clean_data(df):
+     """Clean dataframe by removing duplicates & converting categories from strings 
+    to binary values.
+    
+    Args:
+    df: dataframe. Dataframe containing merged content of messages & categories datasets.
+       
+    Returns:
+    df: dataframe. Dataframe containing cleaned version of input dataframe.
+    """
+     #create a df for each indiviual category 
+    categories = df['categories'].str.split(';', expand = True)
+    #select first row
+    row = categories.iloc[0, :]
+    #get list of categories from that row , apply lambda function untill second to last character
+    category_colnames = row.apply(lambda x:x.split('-')[0]).values.tolist()
+    #rename cols to categories
+    categories.columns = category_colnames
+    # Convert  category numbers
+    for column in categories:
+        # set every value to be the last character of a string
+        categories[column] = categories[column].transform(lambda x: x[-1:])
         
-        # concatenate the data sets
-        df = pd.concat([df, categories], axis=1)
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+    
+    # Drop the  categories  from `df`
+    df.drop('categories', axis = 1, inplace = True)
+    
+    # Concatenate   dataframe with the new `categories` 
+    df = pd.concat([df, categories], axis = 1)
+    # Drop duplicates
+    df.drop_duplicates(inplace = True)
+    # Remove rows with a  value of 2 from df
+    df = df[df['related'] != 2]
+    
+    return df
+
+
+def save_data(df, database_filename):
+    """Save into  SQLite database.
+    
+    inputs:
+    df: dataframe. Dataframe containing cleaned version of merged message and 
+    categories data.
+    database_filename: string. Filename for output database.
+       
+    outputs:
+    None
+    """
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('Messages', engine, index=False, if_exists='replace')
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('Messages', engine, index=False, if_exists='replace')
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
         
-        # drop duplicates
-        df.drop_duplicates(inplace=True)
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
         
-        # save the clean dataset into an sqlite database
-        engine = create_engine('sqlite:///'+db_path)
-        df.to_sql('disastertab', engine, index=False)
-        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages & categories '\
+              'datasets as the first & second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
 if __name__ == '__main__':
     main()
